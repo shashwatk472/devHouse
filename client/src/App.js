83f9194b78d91e96d49afbbcbe63605f76f8626f
Shashwat Kushwaha <vasuk8324@gmail.com>
2022-11-11 22:15:08 +0530

    frontend part done

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,62 @@
-import './App.css';
 import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import ProfileForm from './components/profile-form/ProfileForm';
+import AddExperience from './components/profile-form/AddExperience';
+import AddEducation from './components/profile-form/AddEducation';
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
+import NotFound from './components/layout/NotFound';
+import PrivateRoute from './components/routing/PrivateRoute';
 
+// Redux
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { loadUser } from './actions/auth';
+import { LOGOUT } from './actions/types';
+import setAuthToken from './utils/setAuthToken';
 
+import './App.css';
 
-// import { Provider } from 'react-redux';
-// import { store } from './store';
-// import { loadUser } from './actions/auth';
-// import { LOGOUT } from './actions/types';
-// import setAuthToken from './utils/setAuthToken';
+const App = () => {
+  useEffect(() => {
+    // check for token in LS when app first runs
+    if (localStorage.token) {
+      // if there is a token set axios headers for all requests
+      setAuthToken(localStorage.token);
+    }
+    // try to fetch a user, if no token or invalid token we
+    // will get a 401 response from our API
+    store.dispatch(loadUser());
 
-function App() {
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
+  }, []);
+
   return (
-    // <Provider store={store}>
+    <Provider store={store}>
       <BrowserRouter>
-        {/* <Navbar /> */}
+        <Navbar />
 
-        {/* <Alert /> */}
+        <Alert />
 
         <Routes>
-          {/* <Route path='/' element={<Landing />} />
-          <Route path='register' element={<Register />} /> */}
+          <Route path='/' element={<Landing />} />
+          <Route path='register' element={<Register />} />
           <Route path='login' element={<Login />} />
-          {/* <Route path='profiles' element={<Profiles />} />
-          <Route path='profile/:id' element={<Profile />} /> */}
-          {/* <Route
+          <Route path='profiles' element={<Profiles />} />
+          <Route path='profile/:id' element={<Profile />} />
+          <Route
             path='dashboard'
             element={<PrivateRoute component={Dashboard} />}
           />
@@ -48,11 +78,11 @@ function App() {
           />
           <Route path='posts' element={<PrivateRoute component={Posts} />} />
           <Route path='posts/:id' element={<PrivateRoute component={Post} />} />
-          <Route path='/*' element={<NotFound />} /> */}
+          <Route path='/*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-    // </Provider>
+    </Provider>
   );
-}
+};
 
 export default App;
