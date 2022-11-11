92da348ae18bf8b4ce2ebf7ec343dbc99999db8b
Shashwat Kushwaha <vasuk8324@gmail.com>
2022-11-11 21:44:00 +0530

    authentication added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Login from './components/auth/Login';
+
+
+
+// import { Provider } from 'react-redux';
+// import { store } from './store';
+// import { loadUser } from './actions/auth';
+// import { LOGOUT } from './actions/types';
+// import setAuthToken from './utils/setAuthToken';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Provider store={store}>
+      <BrowserRouter>
+        {/* <Navbar /> */}
+
+        {/* <Alert /> */}
+
+        <Routes>
+          {/* <Route path='/' element={<Landing />} />
+          <Route path='register' element={<Register />} /> */}
+          <Route path='login' element={<Login />} />
+          {/* <Route path='profiles' element={<Profiles />} />
+          <Route path='profile/:id' element={<Profile />} /> */}
+          {/* <Route
+            path='dashboard'
+            element={<PrivateRoute component={Dashboard} />}
+          />
+          <Route
+            path='create-profile'
+            element={<PrivateRoute component={ProfileForm} />}
+          />
+          <Route
+            path='edit-profile'
+            element={<PrivateRoute component={ProfileForm} />}
+          />
+          <Route
+            path='add-experience'
+            element={<PrivateRoute component={AddExperience} />}
+          />
+          <Route
+            path='add-education'
+            element={<PrivateRoute component={AddEducation} />}
+          />
+          <Route path='posts' element={<PrivateRoute component={Posts} />} />
+          <Route path='posts/:id' element={<PrivateRoute component={Post} />} />
+          <Route path='/*' element={<NotFound />} /> */}
+        </Routes>
+      </BrowserRouter>
+    // </Provider>
   );
 }
 
